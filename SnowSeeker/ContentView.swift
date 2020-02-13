5bfa10fbd44a335fb2ce3892903304f816ef715d
Jules Lee <jackconsolidated@gmail.com>
2020-02-13 21:23:16 +0800

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Jules Lee on 2/13/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
